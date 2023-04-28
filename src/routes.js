c18fe6551a6b4049a0bc6dab3b792b91bc844661
Gabriel Assys Mendes Oliveira <110845125+GABRIEL-ASSYS@users.noreply.github.com>
2023-04-28 17:56:12 -0300

    adicionando componente Banner

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SobreMim from "./paginas/SobreMim";
+import Inicio from "./paginas/inicio";
+import Menu from "./componentes/Menu";
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Inicio/>} />
+        <Route path="/sobremim" element={<SobreMim/>} />
+        <Route path="*" element={<div>Página não encontrada !</div>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
